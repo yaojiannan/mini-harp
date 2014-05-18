1066c0e17c46cfd791b36c4496fe1148f199e12b
yaojiannan <Yaojiannan@Yaojiannans-MacBook-Pro.local>
2014-05-18 12:39:22 +0900

    Implement A Current Time Middleware

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ var connect = require('connect');
 
 module.exports = function(dir){
 	var app = connect();
+
+	app.use(function(request, response, next){
+		var url = request.url;
+
+		if (url == "/current-time"){
+			response.end((new Date()).toISOString());
+		} else {
+			next();
+		}
+	})
 	return app;
 }
